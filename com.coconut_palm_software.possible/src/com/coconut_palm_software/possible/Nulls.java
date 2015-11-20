8974492d67208b48d948d64e6768e858da8694cc
David Orme <dorme@bradsdeals.com>
2015-11-20 17:15:13 -0600

    Fixed spelling

--- com.coconut_palm_software.possible/src/com/coconut_palm_software/possible/Nulls.java
+++ com.coconut_palm_software.possible/src/com/coconut_palm_software/possible/Nulls.java
@@ -74,7 +74,7 @@ public class Nulls {
 	 * @param value The value to wrap.
 	 * @return Possible.value(value) iff value != null or Possible.emptyValue() otherwise.
 	 */
-	public static <T> Possible<T> posible(T value) {
+	public static <T> Possible<T> possible(T value) {
 		if (value != null) return Possible.value(value);
 		else return Possible.emptyValue();
 	}
